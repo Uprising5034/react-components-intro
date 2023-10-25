2566b840850ca9a79409868fe556c46f7959eb20
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-10-25 13:10:18 +0100

    done

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,15 @@
+import Buttons from "./Buttons";
+import Image from "./Image";
+import Input from "./Input";
+
+function Main() {
+  return (
+    <main>
+      <Image></Image>
+      <Input></Input>
+      <Buttons></Buttons>
+    </main>
+  );
+}
+
+export default Main;
